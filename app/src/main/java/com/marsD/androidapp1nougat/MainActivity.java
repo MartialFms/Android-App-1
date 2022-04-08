9ddd02aa9cb989674480d8beae7c984084c1fc0b
Promo_01 Stagiaires05 <Stagiaires05P@ad.fms-ea.com>
2022-04-08 20:08:07 +0200

    v0.1

---
+++ app/src/main/java/com/marsD/androidapp1nougat/MainActivity.java
@@ -0,0 +1,14 @@
+package com.marsD.androidapp1nougat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
